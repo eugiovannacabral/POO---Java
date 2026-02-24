26fe217035ce9e72402ffaf5e3a74b53728e8924
Giovanna Cabral <giovannacabralrmesquita@gmail.com>
2026-02-23 21:05:31 -0300

    fiz o ex2

--- aula_introducao.java
+++ aula_introducao.java
@@ -11,7 +11,7 @@ public class aula_introducao {
         System.out.println(nome.replace("Inatel", "Instituto"));
         */
 
-        int horas = 3;
+        /*int horas = 3;
         int primeiraHora = 10;
         int segundaHora = 4;
         int terceiraHora = 2;
@@ -21,6 +21,24 @@ public class aula_introducao {
 
         System.out.println("Total: " + soma);
         System.out.println("Média: " + media);
+        */
+
+        int quantInt = 8; //ou coloca 'var' pois este faz inferencia de tipos como se fosse a String em Python
+        int quantMeia = 5;
+
+        double valorInt = 30.00;
+        double valorMeia = 15.00;
+
+        double totalInt = quantInt * valorInt;
+        double totalMeia = quantMeia * valorMeia;
+        double totalSessao = totalInt + totalMeia;
+
+        double media = ((totalInt) + (totalMeia)) / 13;
+
+        System.out.println(totalInt);
+        System.out.println(totalMeia);
+        System.out.println(totalSessao);
+        System.out.println(media);
     }
-    }
+    
 }

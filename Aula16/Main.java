8dc5b8c093f4b5d8057a026f814a23c2c6288015
Giovanna Cabral <giovannacabralrmesquita@gmail.com>
2026-04-01 22:50:29 -0300

    .

--- Aula16/Main.java
+++ Aula16/Main.java
@@ -1,7 +1,25 @@
 package Aula16;
+import Aula16.Matematica.*;
+import Aula16.Matematica2.*;
 
 public class Main {
     public static void main(String[] args) {
-        Aula16.Matematica.Matematica.Matematica matematica = new Aula16.Matematica.Matematica.Matematica();
+        
+        // .Matematica.Matematica mat = new Aula16.Matematica.Matematica();
+
+        // Aula16.Matematica2.Matematica2 mat2 = new Aula16.Matematica2.Matematica2();
+
+        //outro jeito de fazer só que importando para ficar mais facil, logo o jeito de criar objs fica igual faziasse antes
+
+        Matematica mat1 = new Matematica();
+        Matematica mat2 = new Matematica();
+
+        System.out.println(mat1.soma(1,2));
+        System.out.println(mat1.divisao(2,0));
+        System.out.println(mat1.divisao(10,2));
+
+        mat2.soma(1,2);
+        mat2.divisao(2,1);
+        mat2.divisao(10,2);
     }
 }
--- Aula16/Matematica2/Matematica2.java
+++ Aula16/Matematica2/Matematica2.java
@@ -1,12 +1,12 @@
 package Aula16.Matematica2;
 
-public class Matematica {
+public class Matematica2 {
     
     public void soma(int a, int b){
         System.out.println(a+b);
     }
 
-    public int  divisao(int a, int b){
+    public void divisao(int a, int b){
         int resultado = a/b;
 
         System.out.println(resultado);
